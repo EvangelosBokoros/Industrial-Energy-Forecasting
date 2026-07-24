7f745b5a4166e0082526ad41dd05fb83b490994a
Evangelos Bokoros <vbokoros@gmail.com>
2026-07-24 16:04:35 +0300

    Add project settings

---
+++ src/settings.py
@@ -0,0 +1,19 @@
+DATE_COL = "Ημ/νία"
+
+TARGET_COL = "active_energy_kWh"
+
+FEATURE_COLUMNS = [
+    "total_kg",
+    "total_nominal_kg",
+    "total_brix_units",
+    "total_hours",
+    "total_pallets",
+    "orders",
+    "avg_brix",
+    "yield_ratio_actual_over_nominal",
+    "weekday",
+    "is_weekend",
+    "month",
+    "year",
+    "week_of_year",
+]
